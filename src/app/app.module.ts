345b7f76643e9194e9837ad20b3cde32e9e8a766
Juan Camilo <jc.castiblancor@uniandes.edu.co>
2019-11-07 09:58:13 -0500

    Conexion con back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,15 +18,12 @@ import { IncidenteModule } from './incidente/incidente.module';
 import { EmpleadoModule } from './empleado/empleado.module';
 
 import { ReactiveFormsModule } from "@angular/forms";
-import { CoordinadorClientService } from './coordinador-client.service';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        HttpClientInMemoryWebApiModule.forRoot(CoordinadorClientService, { delay: 100 }),
         ReactiveFormsModule,
         CoordinadorModule,
         IncidenteModule,
@@ -54,7 +51,6 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
             useClass: HttpErrorInterceptor,
             multi: true
         },
-        CoordinadorClientService
     ]
 })
 export class AppModule { }
