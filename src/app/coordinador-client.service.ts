a433d64efa582dd8d6e76a1207827d93b9160611
Juan Camilo <jc.castiblancor@uniandes.edu.co>
2019-11-07 08:07:35 -0500

    Conexion incidente con BD

--- src/app/coordinador-client.service.ts
+++ src/app/coordinador-client.service.ts
@@ -145,6 +145,40 @@ export class CoordinadorClientService implements InMemoryDbService {
 
     ];
     let incidentes = [
+      {
+        "id": 1,
+        "fechaHoraInicial": "fecha1",
+        "fechaHoraFinal": "fecha2",
+        "descripcion": "descripcion1",
+        "observaciones": "observacion1",
+        "calificacion": 1,
+        "categoria": "categoria1",
+        "prioridad": "prioridad1",
+        "solucionado": "true",
+        "reabrir": "false",
+        "equipo": "equipo1",
+        "tecnico": "tecnico1",
+        "empleado": "empleado1",
+        "coordinador": "coordinador1",
+        "actuaciones": []
+    },
+    {
+        "id": 2,
+        "fechaHoraInicial": "fecha3",
+        "fechaHoraFinal": "fecha4",
+        "descripcion": "descripcion2",
+        "observaciones": "observacion2",
+        "calificacion": 1,
+        "categoria": "categoria2",
+        "prioridad": "prioridad2",
+        "solucionado": "true",
+        "reabrir": "false",
+        "equipo": "equipo2",
+        "tecnico": "tecnico2",
+        "empleado": "empleado2",
+        "coordinador": "coordinador2",
+        "actuaciones": []
+    }
     ];
     let tecnicos = [
     ];
