60201e274e0a0df9c1c9aa2cd247e7487440a7e0
Juan Camilo <jc.castiblancor@uniandes.edu.co>
2019-12-04 23:20:57 -0500

    Filtros y ordenamiento coord-incidentes

--- src/app/coordinador/coordinador-incidentes/coordinador-incidentes.component.ts
+++ src/app/coordinador/coordinador-incidentes/coordinador-incidentes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CoordinadorDetail } from "../coordinador-detail";
 import { CoordinadorService } from "../coordinador.service";
+import { Incidente } from '../../incidente/incidente';
 
 @Component({
   selector: 'app-coordinador-incidentes',
@@ -11,7 +12,7 @@ import { CoordinadorService } from "../coordinador.service";
 export class CoordinadorIncidentesComponent implements OnInit {
 
   coordinadorDetail: CoordinadorDetail;
-
+  incidentes: Incidente[];
   id: number;
 
   constructor(
@@ -28,6 +29,34 @@ export class CoordinadorIncidentesComponent implements OnInit {
 
   getCoordinadoreDetail(): void {
     this.coordinadorService.getDetail(this.id).subscribe(c => this.coordinadorDetail = c);
+    this.coordinadorService.getDetail(this.id).subscribe(c => this.incidentes = c.incidentes);
   }
 
+  getIncidentes(): void {
+    this.incidentes = this.coordinadorDetail.incidentes;
+  }
+
+  filtrarPorEspecialidad(): void {
+    this.getIncidentes();
+    var estado: string = (<HTMLInputElement>document.getElementById("filtroEspecialidad")).value;
+    let incidentesFiltrados: Incidente[] = this.buscarIncidentePorEspecialidad(estado, this.incidentes);
+    if (estado.localeCompare('sin filtros') != 0) {
+      this.incidentes = incidentesFiltrados;
+    }
+  }
+
+  ordenar(): void {
+    this.incidentes = this.incidentes.sort((a, b) => b.calificacion - a.calificacion);
+  }
+
+  buscarIncidentePorEspecialidad(text: string, incidentes: Incidente[]): Incidente[] {
+    return text === ""
+      ? incidentes
+      : incidentes.filter(i => i.categoria.includes(text));
+  }
+
+  verEstado(text: string): void {
+    console.log("revisando estado del filtro");
+    console.log(text);
+  }
 }
